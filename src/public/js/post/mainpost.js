d22a066b033130142bc938146df16e62f26e34c6
Hoàng Thiện <thiend17pm03@gmail.com>
2021-04-28 20:33:51 +0700

    add post

--- src/public/js/post/mainpost.js
+++ src/public/js/post/mainpost.js
@@ -11,10 +11,20 @@ function resizeNineScrollLeft() {
   $(".left").getNiceScroll().resize();
 }
 
+function nineScrollRight() {
+  $('.right').niceScroll({
+    smoothscroll: true,
+    horizrailenabled: false,
+    cursorcolor: '#ECECEC',
+    cursorwidth: '7px',
+    scrollspeed: 50
+  });
+}
+
 $(document).ready(function() {
    
   // Cấu hình thanh cuộn
   nineScrollLeft();
-
+  nineScrollRight();
 
 });
