a4b32e4454b6fdd4ef4086939a45b93963f6f834
thiend17pm03 <thiend17pm03@gmail.com>
2021-06-10 14:53:37 +0700

    Update env.js

--- env/env.js
+++ env/env.js
@@ -17,18 +17,18 @@ const env = {
 
   // config admin email account
   MAIL_USER : 'thiend17pm03@gmail.com',
-  MAIL_PASSWORD : '11012000',
+  MAIL_PASSWORD : '',
   MAIL_HOST : 'smtp.gmail.com',
   MAIL_PORT : '587',
 
   //config facebook login app
-  FB_APP_ID :'776098003021595' ,
-  FB_APP_SECRET : '552ccbaf1ceb2498dd8835348ac52d2a',
+  FB_APP_ID :'' ,
+  FB_APP_SECRET : '',
   FB_CALLBACK_URL : 'https://localhost:8000/auth/facebook/callback' ,
 
   //config google login app
-  GG_APP_ID :'366226866454-32lllr6v7pli3c01rip3mv8ef23jkgj0.apps.googleusercontent.com' ,
-  GG_APP_SECRET : '_4z-cj5Pv14QNT7rUgOz-8UZ',
+  GG_APP_ID :'' ,
+  GG_APP_SECRET : '',
   GG_CALLBACK_URL : 'https://localhost:8000/auth/google/callback' ,
 
 }
